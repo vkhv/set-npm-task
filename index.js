d5e1db360dc08f308d74039fe9bd5d63364fb721
Владислав А. Хвостов <khvostov@yamoney.ru>
2017-03-09 12:49:29 +0300

    jsdock

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const jsonfile = require('jsonfile'); jsonfile.spaces = 2;
 const pathToRoot = __dirname.split('node_modules')[0];
+
+/**
+ * Added task in package json.
+ * @param {string} taskName - task name.
+ * @param {string} command - comand.
+ *
+ */
 module.exports = (taskName, command) => {
 	const pathToRootPackageJson = path.join(pathToRoot, 'package.json');
 	jsonfile.readFile(path.join(pathToRoot, 'package.json'), (err, packageJson) => {
