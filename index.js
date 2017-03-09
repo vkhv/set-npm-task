f88e38d71cbaa6be2582604fc692f450ad12d712
Владислав А. Хвостов <khvostov@yamoney.ru>
2017-03-09 11:50:30 +0300

    init

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const path = require('path');
 const jsonfile = require('jsonfile'); jsonfile.spaces = 2;
-
+const pathToRoot = __dirname.split('node_modules')[0];
 module.exports = (taskName, command) => {
 	const pathToRootPackageJson = path.join(pathToRoot, 'package.json');
-
 	jsonfile.readFile(path.join(pathToRoot, 'package.json'), (err, packageJson) => {
 		if (err) throw err;
-
 		packageJson[taskName] = command;
 		jsonfile.writeFileSync(pathToRootPackageJson, packageJson);
 	});
